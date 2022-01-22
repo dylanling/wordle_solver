e03e6e36ea57cc5eb4ada59d016145414e5ff39d
Dylan Ling <dylan.ling@chime.com>
2022-01-21 18:46:31 -0800

    lmao git add

--- solve.py
+++ solve.py
@@ -1,5 +1,6 @@
 import sys
 from wordle import Wordle
+from strategy import Strategy, SmolBrain
 
 word_file = sys.argv[1] if len(sys.argv) > 1 else "wordle-answers-alphabetical.txt"
 with open(word_file) as f:
@@ -7,13 +8,14 @@ with open(word_file) as f:
 
 size = 5
 wordle = Wordle(possible_words, size)
+strategy = SmolBrain(wordle)
 
-while len(wordle.possible_words) != 1:
-  suggestion = wordle.suggest()
+while len(strategy.wordle.possible_words) != 1:
+  suggestion = strategy.suggestion()
   print(f"Suggested guess is: {suggestion.upper()}")
   result = input(f"What was the result for {suggestion.upper()}?\n")
   if result.lower() == "q" or result == "1" * size:
     sys.exit()
-  wordle.update(suggestion, result)
+  strategy.wordle.update(suggestion, result)
 
-print(f"Only remaining word is: {wordle.suggest()}")
+print(f"Only remaining word is: {strategy.suggestion()}")
